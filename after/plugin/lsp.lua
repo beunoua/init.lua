beunoua/init.lua

commit f8e1a22e5c93ca16db2df5d9765d520ef139b59e

remove pylsp, now using ruff

after/plugin/lsp.lua
@@ -1,11 +1,11 @@
 local lsp = require('lsp-zero')
 
-require("mason").setup()
+require("mason").setup({})
 require("mason-lspconfig").setup({
     ensure_installed = {
         "clangd",
         "lua_ls",
-        "pylsp",
+        "ruff",
         "rust_analyzer",
         "yamlls",
     }
@@ -54,18 +54,10 @@ lsp_config.lua_ls.setup({
     },
 })
 
-lsp_config.pylsp.setup({
+lsp_config.ruff.setup({
     on_attach = on_attach,
-    settings = {
-        pylsp = {
-            plugins = {
-                black = { enabled = true },
-                isort = { enabled = true, profile = "black" },
-                pylint = { enabled = true, },
-                pycodestyle = {
-                    maxLineLength=100,
-                },
-            },
+    init_options = {
+        settings = {
         },
     },
 })

commit f983fdc943fab260084829260b584359bf82c549

fixes bug introduced in f8e1a22

after/plugin/lsp.lua
@@ -58,9 +58,11 @@ lsp_config.ruff.setup({
     on_attach = on_attach,
     init_options = {
         settings = {
-        },
-    },
+          lineLength = 100
+        }
+    }
 })
+
 
 lsp_config.clangd.setup({
     on_attach = function(client, bufnr)